50b5d6ba08ac0147c0f32d70dfb2b5beecfeb0a7
aguanchamedina <103070625+aguanchamedina@users.noreply.github.com>
2022-04-25 09:39:51 -0500

    Add files via upload

---
+++ PAR_IMPAR/par_impar.py
@@ -0,0 +1,13 @@
+#Determinar si un numero es par o impar 
+
+#input
+n = int(input("Ingresa un numero: "))
+
+#Processing 
+if n % 2 == 0:
+    print("El numero " , n, "es par")
+
+else: 
+    print("El numero " , n, " es impar")
+
+#Output 
